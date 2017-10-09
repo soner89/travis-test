0f570bc644cc7ea384fce29fdc78ee8c6e0c0252
Soner Kalayci <soner.kalayci@zoi.de>
2017-10-09 11:55:57 +0200

    Removed push.sh - not needed

--- push.sh
+++
@@ -1,9 +0,0 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name  "Travis CI"
-
-git checkout master
-git add .
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-
-git remote add origin https://${GH_TOKEN}@github.com/soner89/travis-test.git > /dev/null 2>&1
-git push --set-upstream origin master
