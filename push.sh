3fbb12e2b19543295ab626b95175b0e7e8ebf8da
Soner Kalayci <soner.kalayci@zoi.de>
2017-10-09 11:26:29 +0200

    Added push script for Travis

---
+++ push.sh
@@ -0,0 +1,9 @@
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name  "Travis CI"
+
+git checkout -b master
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin-pages https://${GH_TOKEN}@github.com/soner89/travis-test > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages gh-pages
